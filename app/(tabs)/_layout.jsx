b5cb623fa8130a08a5b027f0b894abeacb5814d6
Kilativ54 <vitalikechkalov@gmail.com>
2024-11-05 13:24:31 +0100

    start working with icons for tabs

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,6 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { Tabs, Redirect } from "expo-router";
 
+const TabIcon = ({ icon, focused, color, backgroundColor, width, height }) => {
+  return (
+    <View style={styles.tabBox}>
+      <Image
+        width={width}
+        height={height}
+        source={icon}
+        style={{ width: 40, height: 40 }}
+      />
+    </View>
+  );
+};
 const TabsLayout = () => {
   return (
     <>
@@ -8,22 +20,31 @@ const TabsLayout = () => {
         <Tabs.Screen
           name="home"
           options={{
-            title: "Home",
+            title: "",
             headerShown: false,
+            tabBarIcon: () => (
+              <TabIcon icon={require("../../assets/icons/homeNotFocused.png")} />
+            ),
           }}
         />
         <Tabs.Screen
           name="create"
           options={{
-            title: "Create",
+            title: "",
             headerShown: false,
+            tabBarIcon: () => (
+              <TabIcon width={13} height={13} icon={require("../../assets/icons/Union.png")} />
+            ),
           }}
         />
         <Tabs.Screen
           name="profile"
           options={{
-            title: "Profile",
+            title: "",
             headerShown: false,
+            tabBarIcon: () => (
+              <TabIcon width={20} height={20} icon={require("../../assets/icons/profile.png")} />
+            ),
           }}
         />
       </Tabs>
@@ -33,4 +54,13 @@ const TabsLayout = () => {
 
 export default TabsLayout;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  tabBox: {
+    width: 70,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "#FF6C00",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
